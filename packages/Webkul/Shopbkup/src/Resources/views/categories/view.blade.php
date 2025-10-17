dc4476e6258a08aca6d8e26fa5b435abdaf9ec0d
Kavindu Lakshitha <kavindu.lakshitha.ofc@gmail.com>
2025-10-17 10:00:35 +0530

    Card feature

--- packages/Webkul/Shopbkup/src/Resources/views/categories/view.blade.php
+++ packages/Webkul/Shopbkup/src/Resources/views/categories/view.blade.php
@@ -124,7 +124,7 @@
                         <div v-else class="mt-8 max-md:mt-5">
                             <!-- Product Card Shimmer Effect -->
                             <template v-if="isLoading">
-                                <div class="grid grid-cols-3 gap-8 max-1060:grid-cols-2 max-md:justify-items-center max-md:gap-x-4">
+                                <div class="grid grid-cols-4 gap-6 max-1180:grid-cols-3 max-1060:grid-cols-2 max-md:grid-cols-1 max-md:justify-items-center max-md:gap-x-4">
                                     <x-shop::shimmer.products.cards.grid count="12" />
                                 </div>
                             </template>
@@ -134,7 +134,7 @@
                             <!-- Product Card Listing -->
                             <template v-else>
                                 <template v-if="products.length">
-                                    <div class="grid grid-cols-3 gap-8 max-1060:grid-cols-2 max-md:justify-items-center max-md:gap-x-4">
+                                    <div class="grid grid-cols-4 gap-6 max-1180:grid-cols-3 max-1060:grid-cols-2 max-md:grid-cols-1 max-md:justify-items-center max-md:gap-x-4">
                                         <x-shop::products.card
                                             ::mode="'grid'"
                                             v-for="product in products"
