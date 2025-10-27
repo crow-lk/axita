6688a93ccae91e95b4db28880441c9e0f838dfc9
Kavindu Lakshitha <kavindu.lakshitha.ofc@gmail.com>
2025-10-27 14:52:00 +0530

    header styling issues

--- packages/Webkul/Shop/src/Resources/views/components/layouts/header/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/header/index.blade.php
@@ -14,24 +14,4 @@
 <!-- Add spacing to prevent content jump -->
 <div id="header-spacer"></div>
 
-<!-- Spacer for sticky header -->
-<script>
-    window.addEventListener('load', function() {
-        const header = document.getElementById('main-header');
-        const spacer = document.getElementById('header-spacer');
-        if (header && spacer) {
-            spacer.style.height = header.offsetHeight + 'px';
-        }
-    });
-                header.style.transform = 'translateY(-100%)';
-            } else {
-                // Scrolling up - show header
-                header.style.transform = 'translateY(0)';
-            }
-
-            lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-        }, false);
-    })();
-</script>
-
 {!! view_render_event('bagisto.shop.layout.header.after') !!}
